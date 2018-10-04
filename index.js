58de4f7d14bd26ea7fb67160fe45f9844dbd2a6c
joshsarna <joshsarna@gmail.com>
2018-10-04 16:52:19 -0700

    install vue

---
+++ index.js
@@ -0,0 +1,40 @@
+/* global Vue, VueRouter, axios */
+
+var TIL = {
+  template: "#til",
+  data: function() {
+    return {
+      message: "Welcome to Vue.js!"
+    };
+  },
+  created: function() {},
+  methods: {},
+  computed: {}
+};
+
+var HomePage = {
+  template: "#home-page",
+  data: function() {
+    return {
+      message: "Welcome to Vue.js!"
+    };
+  },
+  created: function() {},
+  methods: {},
+  computed: {}
+};
+
+var router = new VueRouter({
+  routes: [
+    { path: "/", component: HomePage },
+    { path: "/til", component: TIL }
+  ],
+  scrollBehavior: function(to, from, savedPosition) {
+    return { x: 0, y: 0 };
+  }
+});
+
+var app = new Vue({
+  el: "#vue-app",
+  router: router
+});
