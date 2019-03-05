d8339a4e517ae0bb2ef4ed8b979e344bca2be21a
joshsarna <joshsarna@gmail.com>
2019-03-04 22:59:20 -0800

    add gem descriptions

--- index.js
+++ index.js
@@ -10,21 +10,21 @@ var Gems = {
           name: "red_book",
           rubygemsUrl: "https://rubygems.org/gems/red_book",
           githubUrl: "https://github.com/joshsarna/red_book",
-          description: "",
+          description: "This gem prints random Lord of the Rings quotes to the rails server every time a route is hit. There are methods to generate random quotes at desire, as well, both from the books and from the movie.",
           currentVersion: "0.0.17"
         },
         {
           name: "utsless",
           rubygemsUrl: "https://rubygems.org/gems/utsless",
           githubUrl: "https://github.com/joshsarna/utsless",
-          description: "",
-          currentVersion: "0.0.3"
+          description: "I originally made this gem as a joke. The idea came when I was going over the differences between 'puts', 'p', and 'return' with students learning rails: return in methods, p when debugging, and never puts again. I created utsless, a gem that executes all puts statements as p statements. That is still the core of its functionality, but I've added other features that allow the user to print relevant information about objects, like their class trees.",
+          currentVersion: "0.0.5"
         },
         {
           name: "fellowship",
           rubygemsUrl: "https://rubygems.org/gems/fellowship",
           githubUrl: "https://github.com/joshsarna/fellowship",
-          description: "",
+          description: "This is a set of tools for solving algorithm problems. I found myself using the same methods repeatedly while working my way through Project Euler, and I wanted to codify and DRY.",
           currentVersion: "0.0.1"
         }
       ]
